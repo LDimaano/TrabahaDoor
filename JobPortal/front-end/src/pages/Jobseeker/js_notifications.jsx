92719c120173fd09f7701036da26f0b65d9cd6be
carla-dimaandal <21-06804@g.batstate-u.edu.ph>
2024-09-21 13:37:13 +0800

    jobseeker notification page backend

--- JobPortal/front-end/src/pages/Jobseeker/js_notifications.jsx
+++ JobPortal/front-end/src/pages/Jobseeker/js_notifications.jsx
@@ -46,7 +46,7 @@ const Notifications = () => {
     if (!userId) return;
 
     try {
-      const response = await fetch(`http://localhost:5000/api/notifications`, {
+      const response = await fetch(`http://localhost:5000/api/jsnotifications`, {
         method: 'GET',
         credentials: 'include',
         headers: {
