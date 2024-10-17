6fc9b5f7954b60d42a531f8e220733d703717c0f
LDimaano <21-09169@g.batstate-u.edu.ph>
2024-10-17 14:31:47 +0800

    "Added state to toggle sidebar visibility, updated sidebar and mobile sidebar logic"

--- JobPortal/front-end/src/pages/Employer/emp_timetofill.jsx
+++ JobPortal/front-end/src/pages/Employer/emp_timetofill.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/emp_header';
 import SideBar from '../../components/emp_side';
 import BarChart from '../../components/emp_barchart';
 
 function DashboardAnalytics() {
+  const [isSidebarVisible, setSidebarVisible] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!isSidebarVisible);
+  };
+
   return (
     <div className="container-fluid">
       <div className="row flex-nowrap">
         {/* Sidebar */}
-        <div className="col-auto p-0 d-none d-lg-block">
+        <div className={`col-auto p-0 d-lg-block ${isSidebarVisible ? 'd-block' : 'd-none'}`}>
           <SideBar />
         </div>
 
         {/* Main content */}
         <div className="col">
-          {/* Page Title */}
+          {/* Header */}
           <div className="py-3">
             <Header />
           </div>
@@ -30,15 +36,19 @@ function DashboardAnalytics() {
       <button
         className="btn btn-primary d-lg-none position-fixed"
         style={{ top: '10px', left: '10px', zIndex: 999 }}
-        onClick={() => document.querySelector('.sidebar-mobile').classList.toggle('d-none')}
+        onClick={toggleSidebar}
       >
         <i className="fa fa-bars"></i>
       </button>
-
-      {/* Mobile Sidebar */}
-      <div className="sidebar-mobile d-none d-lg-none position-fixed bg-white" style={{ top: '0', left: '0', height: '100%', zIndex: 998, width: '250px' }}>
-        <SideBar />
-      </div>
+      
+      {/* Overlay for sidebar on mobile */}
+      {isSidebarVisible && (
+        <div
+          className="position-fixed w-100 h-100"
+          style={{ top: 0, left: 0, backgroundColor: 'rgba(0,0,0,0.5)', zIndex: 998 }}
+          onClick={toggleSidebar}
+        />
+      )}
     </div>
   );
 }
