8ce7825003e1394c60cff88c88d2faf9486101d2
carla-dimaandal <21-06804@g.batstateu.edu.ph>
2024-10-27 09:50:02 +0800

    "Replaced Similarity Score with Rating display using filled and unfilled stars"

--- JobPortal/front-end/src/components/joblistitem.jsx
+++ JobPortal/front-end/src/components/joblistitem.jsx
@@ -29,7 +29,9 @@ function JobListItem({ job }) {
           <p style={itemStyle}>{job.industry_name}</p>
           <p style={itemStyle}>{job.salaryrange}</p> 
           <Tag>{job.jobtype}</Tag> {/* Use the Tag component for jobtype */}
-          <p style={itemStyle}>Similarity Score: {job.similarity_score}</p>
+          <p style={itemStyle}>
+            Rating: {'★'.repeat(job.star_rating)}{'☆'.repeat(5 - job.star_rating)} {/* Example for filled and unfilled stars */}
+          </p>
           <p style={itemStyle}>Influence Type: {job.influence_tag}</p>
         </div>
       </div>
