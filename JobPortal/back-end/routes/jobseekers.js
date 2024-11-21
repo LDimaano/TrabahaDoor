a9f6bcfda8723d08cdd2d1ef71ebd8832725a687
LDimaano <21-09169@g.batstate-u.edu.ph>
2024-11-22 01:15:21 +0800

    "Updated property access in jobseeker profile update route from 'jobTitleId' to 'jobTitle'"

--- JobPortal/back-end/routes/jobseekers.js
+++ JobPortal/back-end/routes/jobseekers.js
@@ -251,7 +251,7 @@ router.put('/update-jobseeker-profile/:userId', async (req, res) => {
     if (experiences && Array.isArray(experiences)) {
       await pool.query(`DELETE FROM job_experience WHERE user_id = $1`, [userId]);
       for (const exp of experiences) {
-        const jobTitleValue = exp.jobTitleId?.value || null;
+        const jobTitleValue = exp.jobTitle?.value || null;
         await pool.query(`
           INSERT INTO job_experience (user_id, jobtitle_id, salary, company, location, start_date, end_date, description)
           VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
