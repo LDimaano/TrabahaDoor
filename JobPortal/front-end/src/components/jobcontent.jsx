cf6f8bec5f768acbdb38d19c9a870a7741d61dcb
carla-dimaandal <21-06804@g.batstateu.edu.ph>
2024-11-23 09:10:20 +0800

    blue to black

--- JobPortal/front-end/src/components/jobcontent.jsx
+++ JobPortal/front-end/src/components/jobcontent.jsx
@@ -5,7 +5,7 @@ const JobContent = ({ jobdescription, responsibilities, qualifications }) => {
     <div className="col-md-8">
       <section className="mb-4">
         <h2 className="text-primary border-bottom pb-2">Description</h2>
-        <p className="text-muted fs-5">{jobdescription}</p>
+        <p className="text-dark fs-5">{jobdescription}</p> 
       </section>
       <section className="mb-4">
         <h2 className="text-primary border-bottom pb-2">Responsibilities</h2>
@@ -13,7 +13,7 @@ const JobContent = ({ jobdescription, responsibilities, qualifications }) => {
           {responsibilities.map((responsibility, index) => (
             <li key={index} className="list-group-item">
               <i className="me-2 fa fa-check-circle text-success"></i>
-              {responsibility}
+              <span className="text-dark">{responsibility}</span> {/* Changed text color to dark */}
             </li>
           ))}
         </ul>
@@ -24,7 +24,7 @@ const JobContent = ({ jobdescription, responsibilities, qualifications }) => {
           {qualifications.map((qualification, index) => (
             <li key={index} className="list-group-item">
               <i className="me-2 fa fa-check-circle text-success"></i>
-              {qualification}
+              <span className="text-dark">{qualification}</span> {/* Changed text color to dark */}
             </li>
           ))}
         </ul>
