dc719b93d7ef7e6337ed063705f095096eacb6cf
LDimaano <21-09169@g.batstate-u.edu.ph>
2024-12-01 23:31:03 +0800

    "Added console logs for personalData and professionalData in ApplicantProfile component"

--- JobPortal/front-end/src/components/app_profile.jsx
+++ JobPortal/front-end/src/components/app_profile.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import PersonalInfo from './personal_info';
 import ProfessionalInfo from './prof_info';
 
-const ApplicantProfile = ({ personalData, professionalData }) => (
-  <main className="bg-white p-4 border rounded">
-    <div>
-      <PersonalInfo data={personalData} />
-      <hr />
-      <ProfessionalInfo data={professionalData} />
-    </div>
-  </main>
-);
+const ApplicantProfile = ({ personalData, professionalData }) => {
+  console.log('personal data:', personalData);
+  console.log('professional data:', professionalData);
+
+  return (
+    <main className="bg-white p-4 border rounded">
+      <div>
+        <PersonalInfo data={personalData} />
+        <hr />
+        <ProfessionalInfo data={professionalData} />
+      </div>
+    </main>
+  );
+};
+
 
 export default ApplicantProfile;
