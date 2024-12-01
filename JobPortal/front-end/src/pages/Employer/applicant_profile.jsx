3844824bad52922e0acc7f9e2808c6c3aae0850d
LDimaano <21-09169@g.batstate-u.edu.ph>
2024-12-01 23:05:06 +0800

    Added "educations" property to applicant profile data, mapping education names from API response.

--- JobPortal/front-end/src/pages/Employer/applicant_profile.jsx
+++ JobPortal/front-end/src/pages/Employer/applicant_profile.jsx
@@ -156,6 +156,7 @@ const MyProfile = () => {
           gender: data.jobSeeker.gender || 'Not Specified',
           address: data.jobSeeker.address || 'Address not provided',
           industry: data.jobSeeker.industry || 'Industry not provided',
+          educations: data.educations.map(education => education.education_name) || [],
         });
 
         const firstJobExperience = data.jobExperience[0] || {};
