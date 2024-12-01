9e95771353e8333c0fd1a2413a7c927241787a88
LDimaano <21-09169@g.batstate-u.edu.ph>
2024-12-01 23:26:34 +0800

    "Updated PersonalInfo component to accept 'educations' prop and added console log for debugging"

--- JobPortal/front-end/src/components/personal_info.jsx
+++ JobPortal/front-end/src/components/personal_info.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import Tag from './jstag';
 
-const PersonalInfo = ({ data }) => (
-  <div>
-    <h4>Personal Information</h4>
-    <p><strong>Full Name:</strong> {data.fullName}</p>
-    <p><strong>Date of Birth:</strong> {data.dateOfBirth}</p>
-    <p><strong>Gender:</strong> {data.gender}</p>
-    <p><strong>Address:</strong> {data.address}</p>
-    <p><strong>Industry:</strong> {data.industry}</p>
-    <div className="d-flex flex-wrap" style={{ gap: '8px' }}>
-      {data.educations.map((education, index) => (
-        <Tag key={index} style={{ marginBottom: '8px' }}>{education}</Tag>
-      ))}
+const PersonalInfo = ({ data, educations }) => {
+  console.log("the data received:", data);
+
+  return (
+    <div>
+      <h4>Personal Information</h4>
+      <p><strong>Full Name:</strong> {data.fullName}</p>
+      <p><strong>Date of Birth:</strong> {data.dateOfBirth}</p>
+      <p><strong>Gender:</strong> {data.gender}</p>
+      <p><strong>Address:</strong> {data.address}</p>
+      <p><strong>Industry:</strong> {data.industry}</p>
+      <div className="d-flex flex-wrap" style={{ gap: '8px' }}>
+        {data.educations.map((education, index) => (
+          <Tag key={index} style={{ marginBottom: '8px' }}>{education}</Tag>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
 
 export default PersonalInfo;
