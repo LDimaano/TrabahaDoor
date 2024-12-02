ab82f8e3c605b7c47c99d4be019d46441ab7a203
LDimaano <21-09169@g.batstate-u.edu.ph>
2024-12-02 21:52:30 +0800

    "Refactored HomeJobSeeker component to include Header and SearchForm, updated FilterSection and JobList components, and added salary range filter with apply button"

--- JobPortal/front-end/src/pages/Jobseeker/home_jobseeker.jsx
+++ JobPortal/front-end/src/pages/Jobseeker/home_jobseeker.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import FilterSection from './FilterSection';
-import JobList from './JobList';
+import Header from '../../components/jsheader';
+import SearchForm from '../../components/searchform';
+import FilterSection from '../../components/filtersection';
+import JobList from '../../components/joblist';
 import { Range } from 'react-range';
 
 function HomeJobSeeker() {
@@ -8,7 +10,8 @@ function HomeJobSeeker() {
     employmentTypes: [],
     salaryRanges: [],
   });
-
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchType, setSearchType] = useState('jobTitle');
   const [salaryRange, setSalaryRange] = useState([5000, 100000]);
 
   const handleFilterChange = (key, value) => {
@@ -30,60 +33,78 @@ function HomeJobSeeker() {
   };
 
   return (
-    <div className="container">
-      <div className="row">
-        {/* Filter Section */}
-        <div className="col-md-4">
-          <FilterSection onFilterChange={handleFilterChange} />
-          
-          {/* Salary Range Filter */}
-          <div className="filter-group mt-4">
-            <h5>Salary Range</h5>
-            <Range
-              step={1000}
-              min={5000}
-              max={100000}
-              values={salaryRange}
-              onChange={handleSalaryRangeChange}
-              renderTrack={({ props, children }) => (
-                <div
-                  {...props}
-                  style={{
-                    ...props.style,
-                    height: '6px',
-                    width: '100%',
-                    background: '#ccc',
-                  }}
-                >
-                  {children}
-                </div>
-              )}
-              renderThumb={({ props }) => (
-                <div
-                  {...props}
-                  style={{
-                    ...props.style,
-                    height: '20px',
-                    width: '20px',
-                    background: '#007bff',
-                    borderRadius: '50%',
-                  }}
-                />
-              )}
-            />
-            <div className="d-flex justify-content-between">
-              <span>${salaryRange[0]}</span>
-              <span>${salaryRange[1]}</span>
-            </div>
-            <button className="btn btn-primary mt-3" onClick={applySalaryFilter}>
-              Apply
-            </button>
-          </div>
-        </div>
+    <div className="home-jobseeker">
+      {/* Header */}
+      <Header />
 
-        {/* Job List */}
-        <div className="col-md-8">
-          <JobList filters={filters} />
+      {/* Search Form */}
+      <SearchForm
+        searchQuery={searchQuery}
+        searchType={searchType}
+        onSearchChange={(query) => setSearchQuery(query)}
+        onSearchTypeChange={(type) => setSearchType(type)}
+      />
+
+      <div className="container mt-4">
+        <div className="row">
+          {/* Filter Section */}
+          <div className="col-md-4">
+            <FilterSection onFilterChange={handleFilterChange} />
+
+            {/* Salary Range Filter */}
+            <div className="filter-group mt-4">
+              <h5>Salary Range</h5>
+              <Range
+                step={1000}
+                min={5000}
+                max={100000}
+                values={salaryRange}
+                onChange={handleSalaryRangeChange}
+                renderTrack={({ props, children }) => (
+                  <div
+                    {...props}
+                    style={{
+                      ...props.style,
+                      height: '6px',
+                      width: '100%',
+                      background: '#ccc',
+                    }}
+                  >
+                    {children}
+                  </div>
+                )}
+                renderThumb={({ props }) => (
+                  <div
+                    {...props}
+                    style={{
+                      ...props.style,
+                      height: '20px',
+                      width: '20px',
+                      background: '#007bff',
+                      borderRadius: '50%',
+                    }}
+                  />
+                )}
+              />
+              <div className="d-flex justify-content-between">
+                <span>${salaryRange[0]}</span>
+                <span>${salaryRange[1]}</span>
+              </div>
+              <button className="btn btn-primary mt-3" onClick={applySalaryFilter}>
+                Apply
+              </button>
+            </div>
+          </div>
+
+          {/* Job List */}
+          <div className="col-md-8">
+            <JobList
+              filters={filters}
+              searchQuery={searchQuery}
+              searchType={searchType}
+              isRecommended={false} // Set to true if recommended jobs are required
+            />
+          </div>
         </div>
       </div>
     </div>
