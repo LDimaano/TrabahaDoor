d8f56e34a356a3ca37ce7b0e9bb95f9509968ea4
LDimaano <21-09169@g.batstate-u.edu.ph>
2024-12-06 16:23:03 +0800

    "Refactored company size parsing in PersonalInfo component"

--- JobPortal/front-end/src/components/emp_companyinfo.jsx
+++ JobPortal/front-end/src/components/emp_companyinfo.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
-const PersonalInfo = ({ data }) => (
-  <div>
-    <h4>Company Information</h4>
-    <p><strong>Company Name:</strong> {data.company_name}</p>
-    <p><strong>Company Address:</strong> {data.company_address}</p>
-    <p><strong>Industry:</strong> {data.industry}</p>
-    <p><strong>Company Size:</strong> {data.company_size[0]}-{data.company_size[1]} employees</p>
-    <p><strong>Founded Year:</strong> {data.founded_year}</p><hr/>
-    <p><strong>Description:</strong> {data.description}</p>
-  </div>
-);
+const PersonalInfo = ({ data }) => {
+  // Parsing the company_size string into an array of numbers
+  const companySize = data.company_size
+    .replace(/[{}"]/g, '')    // Remove curly braces and quotes
+    .split(',')               // Split by the comma
+    .map(num => num.trim());  // Trim any extra spaces
+
+  return (
+    <div>
+      <h4>Company Information</h4>
+      <p><strong>Company Name:</strong> {data.company_name}</p>
+      <p><strong>Company Address:</strong> {data.company_address}</p>
+      <p><strong>Industry:</strong> {data.industry}</p>
+      <p><strong>Company Size:</strong> {companySize[0]}-{companySize[1]} employees</p>
+      <p><strong>Founded Year:</strong> {data.founded_year}</p><hr/>
+      <p><strong>Description:</strong> {data.description}</p>
+    </div>
+  );
+};
 
 export default PersonalInfo;
