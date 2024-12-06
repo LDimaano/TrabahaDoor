ec5080709ebb616728a56346bb718da9b2440862
carla-dimaandal <21-06804@g.batstateu.edu.ph>
2024-12-06 19:43:25 +0800

    bigger header

--- JobPortal/front-end/src/components/header_unverified1.jsx
+++ JobPortal/front-end/src/components/header_unverified1.jsx
@@ -3,17 +3,22 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Header() {
-return (
+  return (
     <nav className="navbar navbar-expand-lg bg-transparent">
       <div className="container d-flex justify-content-between align-items-center">
         <img
           src={`${process.env.PUBLIC_URL}/assets/TrabahaDoor_logo.png`}
           alt="TrabahaDoor Logo"
-          width="30"
-          height="30"
-          className="me-2"
+          width="40" // Increased size for logo
+          height="40" // Increased size for logo
+          className="me-3"
         />
-        <span className="fw-bold">TrabahaDoor</span>
+        <span
+          className="fw-bold"
+          style={{ fontSize: '1.8rem', letterSpacing: '1px' }} // Bigger font and slight letter spacing
+        >
+          TrabahaDoor
+        </span>
         <button
           className="navbar-toggler"
           type="button"
